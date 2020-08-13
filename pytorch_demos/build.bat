186e4b42bb3ab84d1efa677ac164a97c5711788d
Viktor Yastrebov <v.yastrebov90@gmail.com>
2020-08-13 04:52:41 +0300

    remove pytorch C++ API not finished demo

--- pytorch_demos/build.bat
+++
@@ -1,3 +0,0 @@
-IF NOT EXIST ".\objects64" mkdir objects64
-cd .\objects64
-cmake -D CMAKE_BUILD_TYPE=Release -G"Visual Studio 15 2017 Win64" ..\
--- pytorch_demos/build_debug.bat
+++
@@ -1,3 +0,0 @@
-IF NOT EXIST ".\objects64d" mkdir objects64d
-cd .\objects64d
-cmake -D CMAKE_BUILD_TYPE=Debug -G"Visual Studio 15 2017 Win64" ..\
